407a5cc4a667f47754dd1a0ddf4521a476c5fbd2
Teejaykhaddy <105528211+Teejaykhaddy@users.noreply.github.com>
2022-12-28 20:57:29 -0500

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -11,16 +11,16 @@ sudo yum install java-1.8.0-openjdk-devel -y
 # Download tomcat software and extract it.
 sudo yum install wget unzip -y
 
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.64/bin/apache-tomcat-9.0.64.tar.gz
-sudo tar -xvf apache-tomcat-9.0.64.tar.gz
-sudo rm apache-tomcat-9.0.64.tar.gz
-sudo mv apache-tomcat-9.0.64 tomcat9
-sudo chmod 777 -R /opt/tomcat9
-sudo chown ec2-user -R /opt/tomcat9
-sh /opt/tomcat9/bin/startup.sh
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.0-M1/bin/apache-tomcat-11.0.0-M1.tar.gz
+sudo tar -xvf apache-tomcat-11.0.0-M1.tar.gz
+sudo rm apache-tomcat-11.0.0-M1.tar.gz
+sudo mv apache-tomcat-11.0.0-M1 tomcat11
+sudo chmod 777 -R /opt/tomcat11
+sudo chown ec2-user -R /opt/tomcat11
+sh /opt/tomcat11/bin/startup.sh
 # create a soft link to start and stop tomcat
-sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
-sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
+sudo ln -s /opt/tomcat11/bin/startup.sh /usr/bin/starttomcat
+sudo ln -s /opt/tomcat11/bin/shutdown.sh /usr/bin/stoptomcat
 sudo yum update -y
 starttomcat
 echo "end on tomcat installation"
